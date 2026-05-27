0d90c535cd738ffae4367226da58b0ac6e736d92
tlahsh12 <tlahsh12@naver.com>
2026-05-27 22:21:20 +0900

    カート試作ページ作成

--- demo/src/main/java/com/ec/demo/config/SecurityFilter.java
+++ demo/src/main/java/com/ec/demo/config/SecurityFilter.java
@@ -19,6 +19,8 @@ public class SecurityFilter {
                         "/products/**",
                         "/users",
                         "/users/**",
+                        "/cart",
+                        "/cart/**",
                         "/css/**",
                         "/js/**",
                         "/images/**"
--- demo/src/main/java/com/ec/demo/controller/CartController.java
+++ demo/src/main/java/com/ec/demo/controller/CartController.java
@@ -1,5 +1,22 @@
 package com.ec.demo.controller;
 
-public class CartController {
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+@RequestMapping("/cart")
+public class CartController {
+	
+	
+	@GetMapping
+	public String getCartPage(Model model) {
+		
+		return "cart_page";
+	}
+	
+	
 }
